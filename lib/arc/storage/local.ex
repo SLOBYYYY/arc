020223611206830fb351052b09af94f4d220958b
Máj Szabolcs <szabolcs.maj@goodgrind.tech>
2018-09-28 14:17:42 +0200

    Implemented request_dir for local storage URL queries

--- lib/arc/storage/local.ex
+++ lib/arc/storage/local.ex
@@ -14,7 +14,7 @@ defmodule Arc.Storage.Local do
   end
 
   def url(definition, version, file_and_scope, _options \\ []) do
-    local_path = build_local_path(definition, version, file_and_scope)
+    local_path = build_local_request_path(definition, version, file_and_scope)
 
     url = if String.starts_with?(local_path, "/") do
       local_path
@@ -30,10 +30,18 @@ defmodule Arc.Storage.Local do
     |> File.rm()
   end
 
-  defp build_local_path(definition, version, file_and_scope) do
+  defp build_path(definition, version, file_and_scope, directory_getter) do
     Path.join([
-      definition.storage_dir(version, file_and_scope),
+      directory_getter.(version, file_and_scope),
       Arc.Definition.Versioning.resolve_file_name(definition, version, file_and_scope)
     ])
+  end
+
+  defp build_local_path(definition, version, file_and_scope) do
+    build_path(definition, version, file_and_scope, &definition.storage_dir/2)
+  end
+
+  defp build_local_request_path(definition, version, file_and_scope) do
+    build_path(definition, version, file_and_scope, &definition.request_dir/2)
   end
 end
